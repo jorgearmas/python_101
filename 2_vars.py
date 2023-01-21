e3632a28934e3f75890c8a34fd66ccb06ce9c60d
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-21 04:19:56 -0600

    2_variables

---
+++ 2_vars.py
@@ -0,0 +1,18 @@
+"""
+Variables:
+Una variable es un lugar fisico en memoria en donde se puede alamacenar un valor. Este valor puede ser modificado según la necesidad de nuestro programa. Las variables tienen un identificador para que puedan ser invocadas.
+"""
+
+#declaracion de variable
+my_name = "Luke"
+print(my_name)
+
+#reasignacion dinamica de valor de variable
+my_name = 12 #el valor cambia a: 12
+print(my_name)
+my_name = "Santiago" #el valor cambia a: Santiago
+print(my_name)
+
+#input - ingresar el valor de una variables
+my_name = input("Cual es tu nombre? ")
+print(my_name) #se imprime el valor ingresado con input
