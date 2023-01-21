c7403845275199fabbc09336f78c61d03cbba73f
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-21 05:50:19 -0600

    6_booleans

---
+++ 6_booleans.py
@@ -0,0 +1,16 @@
+"""
+Booleans
+"""
+#variable boolean
+is_a_student = True
+print(is_a_student)
+print(type(is_a_student))
+
+#reasignacion de valor manual
+is_a_student = False
+print(is_a_student)
+
+#reasignacion de valor con operador not
+is_a_student = not is_a_student
+print(is_a_student)
+
