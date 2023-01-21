fd1a46e95998c76791af86a1c686cbdec1606540
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-21 05:39:35 -0600

    5_numbers

---
+++ 5_numbers.py
@@ -0,0 +1,26 @@
+"""
+Numeros:
+    - Enteros (int)
+    - Punto Flotante (float)
+"""
+
+#Enteros
+lives = 3
+print(lives)
+print(type(lives))
+
+#Decimales
+temperature = 12.12
+print(lives)
+print(type(temperature))
+
+#operaciones en variables
+#auto sumar en la misma variable
+lives += 1
+print(lives)
+
+#auto restar en la misma variables
+lives -= 1
+print(lives)
+
+
