19925ee91266af5b2501580a572ed688ebd14ad2
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-21 16:11:28 -0600

    9_comparison_operators

---
+++ 9_comparison_operators.py
@@ -0,0 +1,28 @@
+"""
+Operadores de comparación
+"""
+#mayor que
+print(f"Es 7 mayor que 3? {7>3}")
+
+#menor que
+print(f"Es 5 menor que 6? {5<6}")
+
+#mayor, igual que
+print(f"Es 2 mayor o igual que 1? {2>=1}")
+print(f"Es 2 mayor o igual que 2? {2>=2}")
+
+#menor, igual que
+print(f"Es 3 menor o igual que 6? {3<=6}")
+print(f"Es 3 menor o igual que 3? {3<=3}")
+
+#igualdad
+print(f"Es 6 igual que 6? {6==6}")
+
+#diferencia
+print(f"Es 9 distinto a 5? {6!=5}")
+
+#igualdad de strings
+print(f"Es 'hola' igual a 'hola'? {'hola'=='hola'}")
+
+#diferencia de strings
+print(f"Es 'hola' distinto de 'mundo'? {'hola'!='mundo'}")
