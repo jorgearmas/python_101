b90e3c2c9cec191872d10dbe51f627983a8bf866
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-21 04:11:45 -0600

    1_print

---
+++ 1_print.py
@@ -0,0 +1,13 @@
+"""
+Print:
+Print es una funcion que imprime en consola un mensaje o parametro especifico. 
+"""
+#impresión en consola
+print("Hola esto es Python") #los mensajes se encierran con comillas
+
+#impresion de operaciones matematicas
+print(12+5)
+print(10-5)
+print(2*3)
+print(8/2)
+
