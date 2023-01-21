2bfff681c301d41ae2e8671b9e9060eaf1b51ea9
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-21 15:11:05 -0600

    8_arithmetic_operators

---
+++ 8_arithmetic_operators.py
@@ -0,0 +1,22 @@
+"""
+Operadores aritméticos
+"""
+#suma
+print(f"Suma de (10+10): {10+10}")
+#resta
+print(f"Resta de (10-5): {10-5}")
+#multiplicación
+print(f"Multiplicación de (10*2): {10*2}")
+#división
+print(f"División de (10/2): {10/2}")
+#división - cociente entero
+print(f"División exacta de (10/2): {10//2}")
+#módulo
+print(f"Módulo de (10%2): {10%2}")
+#potencia
+print(f"Potencia de (2**3): {2**3}")
+
+#suma de cadenas
+print(f"Suma de 'hola' + 'mundo': {'hola '+'mundo'}")
+#multiplicación de cadenas
+print(f"Multiplicación de 'hola' tres veces: {'hola'*3}")
